e75ce1a49b02b34900219de226b62860ab9cf464
Aaron Maslen <aaron.maslen@gmail.com>
2022-05-18 14:41:39 +1000

    Added WeaponCategory helpers

---
+++ GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Kingmaker.Enums;
+
+namespace Microsoftenator.Wotr.Common.Util.GameLogic
+{
+    public static class Weapons
+    {
+        public static IEnumerable<WeaponCategory> WeaponCategories =
+            Enum.GetValues(typeof(WeaponCategory)).Cast<WeaponCategory>();
+
+        public static IEnumerable<WeaponSubCategory> WeaponSubGroups =
+            Enum.GetValues(typeof(WeaponSubCategory)).Cast<WeaponSubCategory>();
+       
+        public static IEnumerable<WeaponCategory> GetWeaponCategories(this WeaponSubCategory wsc)
+            => WeaponCategories.Where(wc => wc.GetSubCategories().Contains(wsc));
+
+        public static readonly IReadOnlyDictionary<WeaponSubCategory, IEnumerable<WeaponCategory>> WeaponCategoriesBySubCategory =
+            WeaponSubGroups.ToDictionary(keySelector : Functional.Id, elementSelector: GetWeaponCategories);
+    }
+}
--- Util.cs
+++ Util.cs
@@ -17,6 +17,11 @@ namespace Microsoftenator.Wotr.Common.Util
         public static void IgnoreRef<T1, T2>(ref T1 x, ref T2 y) { return; }
     }
     
+    public static class Extensions
+    {
+        public static string GetName<T>(this T value) where T : struct, System.Enum => Enum.GetName(typeof(T), value);
+    }
+
     public static class TTT_Utils
     {
         public static T Clone<T>(T original) where T : notnull
