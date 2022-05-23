911f0c69ffa41ee6681e1d513ad30d9b55dd96b9
Aaron Maslen <aaron.maslen@gmail.com>
2022-05-23 14:55:30 +1000

    Currently unused helper - UnitFactComponentDelegate.GetOwner()

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kingmaker.EntitySystem.Entities;
+using Kingmaker.UnitLogic;
+
+namespace Microsoftenator.Wotr.Common.Extensions
+{
+    public static class UnitFactComponentDelegateExtensions
+    {
+        public static UnitEntityData GetOwner(this UnitFactComponentDelegate instance) => instance.Owner;
+    }
+}
